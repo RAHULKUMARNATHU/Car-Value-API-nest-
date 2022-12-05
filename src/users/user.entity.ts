5241117c408695eccf5ea306f7b32539efdbeb64
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-05 11:56:43 +0530

    commit :: added log for create, update , remove @ user.entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -11,8 +18,18 @@ export class User {
   @Column()
   password: string;
 
-  
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
 
-  
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
 
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
