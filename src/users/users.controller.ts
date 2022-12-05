775736d6a3b1433cd84e5986a7dc9fb08efbed5d
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-05 16:05:09 +0530

    commit :: Creating , Updating , Removing  , Finding , Filtering Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+
 
 @Controller('auth')
 export class UsersController {
@@ -10,4 +21,26 @@ export class UsersController {
     console.log(body);
     this.usersService.create(body.email, body.password);
   }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  
+@Patch('/:id')
+updateUser(@Param ('id') id : string , @Body() body : UpdateUserDto){
+return this.usersService.update(parseInt(id) , body)
+}
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,35 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new Error('User Not Exists');
+    }
+
+    return user;
+  }
+
+  find(email: string) {
+    return this.repo.findBy({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User Not Found ');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('Oops ! user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
