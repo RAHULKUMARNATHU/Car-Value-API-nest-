405ac811058be237929fa4d046678e6f22d2e15a
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-12-06 22:34:28 +0530

    commit :: created auth services

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
