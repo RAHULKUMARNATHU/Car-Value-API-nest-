b26c8bebd1e4708dfd3904312a0127ec3538277e
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-06 12:42:54 +0530

    commit :: serialize the interceptor

--- src/Interceptors/serialize.interceptor.ts
+++ src/Interceptors/serialize.interceptor.ts
@@ -7,6 +7,8 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
+
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
@@ -15,13 +17,17 @@ export class SerializeInterceptor implements NestInterceptor {
   ): Observable<any> | Promise<Observable<any>> {
     //  Run Something before a request handled
     // by the request handler
-    console.log(' I am running before the handler ', context);
+    // console.log(' I am running before the handler ', context);
+
     return next.handle().pipe(
       map((data: any) => {
         //  Run Something before a response handled
         // by the response handler
 
-        console.log('I am running before the response is sent out ', data);
+        // console.log('I am running before the response is sent out ', data);
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
