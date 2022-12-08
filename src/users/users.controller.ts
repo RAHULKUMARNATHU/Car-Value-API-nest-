aa7fa08b215bf91f513871b8d00fe48c8d289c7a
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-08 14:37:30 +0530

    commit :: SignIng in user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,15 +37,19 @@ export class UsersController {
   }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   // @Serialize(UserDto)
