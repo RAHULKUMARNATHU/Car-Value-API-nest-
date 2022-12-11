c9d27c934eb05d86da8b02234d5ee5599d9a2ace
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-12-11 11:43:13 +0530

    commit :: created  an entity for reports

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
