dbcba887446414b6bd0d34a24d71aecba6e3744a
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-15 17:41:25 +0530

    commit :: Testing the Estimate Logic

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -10,12 +10,20 @@ import { Report } from './report.entity';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  createEstimate(estimateDto: GetEstimateDto) {
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
     return this.repo
       .createQueryBuilder()
-      .select('*')
-      .where('make = :make', { make: estimateDto.make })
-      .getRawMany();
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model =:model', { model })
+      .andWhere('lng =:lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat =:lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year =:year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 
   create(reportDto: CreateReportDto, user: User) {
