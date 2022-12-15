e1a894edbe651cd9b4db75d241a22df320208b69
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-15 15:26:06 +0530

    commit :: removed comments

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,10 +1,7 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
 
 export class AdminGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     if (!request.currentUser) {
       return false;
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module , MiddlewareConsumer } from '@nestjs/common';
-// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
-// import { CurrentUserInterceptor } from './interceptors/current-user-interceptor';
 import { CurrentUserMiddleware } from './Middlewares/current-user.middleware';
 
 
@@ -15,10 +13,7 @@ import { CurrentUserMiddleware } from './Middlewares/current-user.middleware';
   providers: [
     UsersService,
     AuthService,
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useClass: CurrentUserInterceptor,
-    // },
+    
   ],
 })
 export class UsersModule {
