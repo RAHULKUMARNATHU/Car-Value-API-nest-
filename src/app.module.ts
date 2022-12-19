76d51287bb9a507578ccb45470794cc32ce90674
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-19 15:11:58 +0530

    commit :: Providing the cookie key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Report } from './reports/report.entity';
 import { User } from './users/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Report } from './reports/report.entity';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -20,21 +22,35 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         return {
           type: 'sqlite',
           database: config.get<string>('DB_NAME'),
-          entities: [Report, User],
           synchronize: true,
+          entities: [User, Report],
         };
       },
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [Report, User],
-    //   synchronize: true,
-    // }),
     UsersModule,
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY')],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  
-  // Session Cookie 
-  app.use(cookieSession({
-    keys : ['nathu'], 
-    
-  }))
-  
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
